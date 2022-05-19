9e1fdc2c24c51e0ad3258b7d2945997563656394
Gustavo Lodi Vidaletti <gustavo.vidaletti@gmail.com>
2022-05-19 09:28:40 -0300

    feat(Header): New Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React, { Fragment } from 'react';
+import Header from './components/header/Header';
 
 function App() {
-  return <Fragment>
-    
-  </Fragment>;
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
 }
 
 export default App;
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import styles from './Header.scss';
+const { rootClassName } = styles;
 
 export default function Header() {
-  return <header>logo</header>;
+  return <header className={rootClassName}>logo</header>;
 }
